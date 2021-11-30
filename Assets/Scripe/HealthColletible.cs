4069d6951d93cf00c5a1b3183c137d820a5e5882
mashenchen <704951265@qq.com>
2021-11-30 21:36:06 +0800

    ruby

---
+++ Assets/Scripe/HealthColletible.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthColletible : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public GameObject effectParticle;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        RubyController rubyController = collision.GetComponent<RubyController>();
+        if (rubyController != null)
+        {
+
+            if (rubyController.Health < rubyController.maxHealth) { 
+            rubyController.ChangeHealth(1);
+                rubyController.PlaySound(audioClip);
+                Instantiate(effectParticle, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            }
+        }
+        
+    }
+
+}
---
+++ Assets/Scripe/Smart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smart : MonoBehaviour
+{
+    public Transform rubytransform;
+    public Transform ruby;
+    private Vector3 player;
+    public float speed = 1f;
+    private Animator animator;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < ruby.position.x)
+        {
+            
+            animator.SetBool("derict",true );
+           
+        }
+        else
+        {
+            
+            animator.SetBool("derict", false);
+        }
+   //     rubytransform.position = new Vector3(rubytransform.position.x + 1, rubytransform.position.y + 1, 0);
+        transform.position = Vector3.MoveTowards(transform.position, rubytransform.position, 0.03f);
+      //  transform.position =(rubytransform.position);
+    }
+}
